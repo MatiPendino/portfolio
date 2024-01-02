9bb24452aaffa6d9e0191cd1bf48db0ed481a21f
Matías <matiaspendino76@gmail.com>
2024-01-02 07:21:21 -0300

    Next update and little fixes.

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,10 +1,8 @@
 'use client'
 import AOS from 'aos'
 import { useEffect } from 'react'
-import { useTranslation } from '../app/i18n/client'
 
 export function Skill ({ languageName, languageLevel, languageIconPath, lng }) {
-  const {t} = useTranslation(lng)
   useEffect(() => {
     import('aos/dist/aos.css')
     AOS.init()
@@ -13,8 +11,8 @@ export function Skill ({ languageName, languageLevel, languageIconPath, lng }) {
     <div className='col-6 col-lg-3 aos-init aos-animate text-center' data-aos='fade-up' data-aos-duration='1100'>
       <div className='skill-card'>
         <img src={languageIconPath} alt={`${languageName} Icon`} />
-        <p>{languageName}</p>
-        <p>{t('level')}: {languageLevel}</p>
+        <p className='text-'>{languageName}</p>
+        <p className='fst-italic'>{languageLevel}</p>
       </div>
     </div>
   )
